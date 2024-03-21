8cf2c2b8083b6aed38396a0b6fec44fd6dcca8cf
Joseph Palmer <75081147+BlockBuilderJoe@users.noreply.github.com>
2024-03-21 16:52:12 +0000

    Added Typescript workflow

---
+++ ts-starter/scripts/main.ts
@@ -0,0 +1,17 @@
+import { world, system } from "@minecraft/server";
+
+function mainTick() {
+  if (system.currentTick % 100 === 0) {
+    world.sendMessage("Hello starter! Tick: " + system.currentTick);
+    // Fetch the block
+    const block = world.getDimension("overworld").getBlock({ x: 1, y: 2, z: 3 });
+    world.sendMessage(`Block Tags: ${block?.getTags()}`);
+    world.sendMessage(`Block is dirt: ${block?.hasTag("dirt")}`);
+    world.sendMessage(`Block is wood: ${block?.hasTag("wood")}`);
+    world.sendMessage(`Block is stone: ${block?.hasTag("stone")}`);
+  }
+
+  system.run(mainTick);
+}
+
+system.run(mainTick);
