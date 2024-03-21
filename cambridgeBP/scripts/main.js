e7bd944e2a5c233273dd1045e251f2478ba60789
Joseph Palmer <75081147+BlockBuilderJoe@users.noreply.github.com>
2024-03-21 14:53:54 +0000

    Changed servermodule to 1.3.0

--- cambridgeBP/scripts/main.js
+++ cambridgeBP/scripts/main.js
@@ -1,19 +1,11 @@
-import { world } from "@minecraft/server";
-import { playerJoin } from "./player";
+import { world, system } from "@minecraft/server";
 
-//Welcomes the player and defines the player object
-const player = await playerJoin(); 
+function mainTick() {
+  if (system.currentTick % 100 === 0) {
+    world.sendMessage("Hello starter! Tick: " + system.currentTick);
+  }
 
-//Gets the name of the block at the given coordinates.
-function getBlock(x, y, z) {
-    let block = world.getDimension("overworld").getBlock({ x: x, y: y, z: z });
-    return block;
+  system.run(mainTick);
 }
 
-//Checks if the block at the given coordinates is air and sends a message to the player.
-if (getBlock(100, 100, 100).name === "minecraft:air") {
-    player.sendMessage(`The block is ${block.name}`);
-}
-
-
-
+system.run(mainTick);
