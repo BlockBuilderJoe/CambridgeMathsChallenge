731aeeccffdfa6f0497d5c72736e4d942921cd06
Joseph Palmer <75081147+BlockBuilderJoe@users.noreply.github.com>
2024-06-05 16:34:14 +0100

    Added perfect run

--- ts-starter/dist/scripts/main.js
+++ ts-starter/dist/scripts/main.js
@@ -314,18 +314,27 @@ import {
 
 // scripts/perfectRun.ts
 var perfectRun = [
-  {
-    location: { z: 33, y: 94, x: 37 },
-    direction: "south",
-    rodLength: 12,
-    blockName: "yellow_concrete"
-  },
-  {
-    location: { z: 45, y: 94, x: 36 },
-    direction: "west",
-    rodLength: 6,
-    blockName: "green_concrete"
-  }
+  { location: { z: 104, y: 95, x: 30 }, direction: "south", rodLength: 12, blockName: "yellow_concrete" },
+  //1/2
+  { location: { z: 92, y: 95, x: 31 }, direction: "west", rodLength: 6, blockName: "green_concrete" },
+  //1/4
+  { location: { z: 91, y: 95, x: 44 }, direction: "west", rodLength: 8, blockName: "brown_concrete" },
+  //1/3
+  { location: { z: 94, y: 95, x: 53 }, direction: "north", rodLength: 4, blockName: "purple_concrete" },
+  //1/6
+  { location: { z: 99, y: 95, x: 55 }, direction: "west", rodLength: 8, blockName: "brown_concrete" },
+  //2/6
+  { location: { z: 99, y: 95, x: 69 }, direction: "west", rodLength: 24, blockName: "blue_concrete" },
+  //1/1
+  { location: { z: 99, y: 95, x: 93 }, direction: "west", rodLength: 24, blockName: "blue_concrete" },
+  //1/1
+  { location: { z: 95, y: 95, x: 115 }, direction: "east", rodLength: 3, blockName: "lime_concrete" },
+  { location: { z: 94, y: 95, x: 109 }, direction: "east", rodLength: 6, blockName: "green_concrete" },
+  { location: { z: 94, y: 95, x: 103 }, direction: "east", rodLength: 3, blockName: "lime_concrete" },
+  { location: { z: 92, y: 95, x: 99 }, direction: "south", rodLength: 2, blockName: "red_concrete" },
+  { location: { z: 89, y: 95, x: 97 }, direction: "east", rodLength: 4, blockName: "purple_concrete" },
+  { location: { z: 89, y: 95, x: 92 }, direction: "east", rodLength: 2, blockName: "red_concrete" },
+  { location: { z: 89, y: 95, x: 87 }, direction: "east", rodLength: 8, blockName: "brown_concrete" }
 ];
 
 // scripts/rod.ts
