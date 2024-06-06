037f15d057df20627af93268930a351b5d4a0c7c
Joseph Palmer <75081147+BlockBuilderJoe@users.noreply.github.com>
2024-06-06 08:14:44 +0100

    All NPC change from default to win state

--- ts-starter/dist/scripts/main.js
+++ ts-starter/dist/scripts/main.js
@@ -316,25 +316,25 @@ import {
 var perfectRun = [
   { location: { z: 104, y: 95, x: 30 }, direction: "north", rodLength: 12, blockName: "yellow_concrete" },
   //1/2
-  { location: { z: 92, y: 95, x: 31 }, direction: "west", rodLength: 6, blockName: "green_concrete" },
+  { location: { z: 92, y: 95, x: 31 }, direction: "east", rodLength: 6, blockName: "green_concrete" },
   //1/4
-  { location: { z: 91, y: 95, x: 44 }, direction: "west", rodLength: 8, blockName: "brown_concrete" },
+  { location: { z: 91, y: 95, x: 44 }, direction: "east", rodLength: 8, blockName: "brown_concrete" },
   //1/3
-  { location: { z: 94, y: 95, x: 53 }, direction: "north", rodLength: 4, blockName: "purple_concrete" },
+  { location: { z: 94, y: 95, x: 53 }, direction: "south", rodLength: 4, blockName: "purple_concrete" },
   //1/6
-  { location: { z: 99, y: 95, x: 55 }, direction: "west", rodLength: 8, blockName: "brown_concrete" },
+  { location: { z: 99, y: 95, x: 55 }, direction: "east", rodLength: 8, blockName: "brown_concrete" },
   //2/6
-  { location: { z: 99, y: 95, x: 69 }, direction: "west", rodLength: 24, blockName: "blue_concrete" },
+  { location: { z: 99, y: 95, x: 69 }, direction: "east", rodLength: 24, blockName: "blue_concrete" },
   //1/1
-  { location: { z: 99, y: 95, x: 93 }, direction: "west", rodLength: 24, blockName: "blue_concrete" },
+  { location: { z: 99, y: 95, x: 93 }, direction: "east", rodLength: 24, blockName: "blue_concrete" },
   //1/1
-  { location: { z: 95, y: 95, x: 115 }, direction: "east", rodLength: 3, blockName: "lime_concrete" },
-  { location: { z: 94, y: 95, x: 109 }, direction: "east", rodLength: 6, blockName: "green_concrete" },
-  { location: { z: 94, y: 95, x: 103 }, direction: "east", rodLength: 3, blockName: "lime_concrete" },
-  { location: { z: 92, y: 95, x: 99 }, direction: "south", rodLength: 2, blockName: "red_concrete" },
-  { location: { z: 89, y: 95, x: 97 }, direction: "east", rodLength: 4, blockName: "purple_concrete" },
-  { location: { z: 89, y: 95, x: 92 }, direction: "east", rodLength: 2, blockName: "red_concrete" },
-  { location: { z: 89, y: 95, x: 87 }, direction: "east", rodLength: 8, blockName: "brown_concrete" }
+  { location: { z: 95, y: 95, x: 115 }, direction: "west", rodLength: 3, blockName: "lime_concrete" },
+  { location: { z: 94, y: 95, x: 109 }, direction: "west", rodLength: 6, blockName: "green_concrete" },
+  { location: { z: 94, y: 95, x: 103 }, direction: "west", rodLength: 3, blockName: "lime_concrete" },
+  { location: { z: 92, y: 95, x: 99 }, direction: "north", rodLength: 2, blockName: "red_concrete" },
+  { location: { z: 89, y: 95, x: 97 }, direction: "west", rodLength: 4, blockName: "purple_concrete" },
+  { location: { z: 89, y: 95, x: 92 }, direction: "west", rodLength: 2, blockName: "red_concrete" },
+  { location: { z: 89, y: 95, x: 87 }, direction: "west", rodLength: 8, blockName: "brown_concrete" }
 ];
 
 // scripts/rod.ts
