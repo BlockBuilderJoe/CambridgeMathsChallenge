c4fa1e3d9b054f6731bdb429f936304786427c1e
IsaacEM <117664480+IsaacEM@users.noreply.github.com>
2024-08-23 15:49:46 +0100

    scale1 gate case actually fixed

--- ts-starter/scripts/gate.ts
+++ ts-starter/scripts/gate.ts
@@ -163,6 +163,10 @@ export async function closeGate(location: string) {
       //set gates
       overworld.runCommandAsync(`setblock 65 96 180 iron_bars`);
       overworld.runCommandAsync(`setblock 65 96 181 iron_bars`);
+      overworld.runCommandAsync(`setblock 65 96 182 iron_bars`);
+      overworld.runCommandAsync(`setblock 65 96 183 iron_bars`);
+      overworld.runCommandAsync(`setblock 65 97 180 iron_bars`);
+      overworld.runCommandAsync(`setblock 65 97 181 iron_bars`);
       overworld.runCommandAsync(`setblock 65 97 182 iron_bars`);
       overworld.runCommandAsync(`setblock 65 97 183 iron_bars`);
       //clear right gate facing out
